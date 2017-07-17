ec4161c8647efd521cd584d6049cca7767de104d
Thomas Ngo <tngo0508@csu.fullerton.edu>
2017-07-17 21:44:48 +0000

    finish shell

--- shellskel.cpp
+++ shellskel.cpp
@@ -10,42 +10,59 @@ using namespace std;
 
 int main()
 {
-	/* The command buffer */
-	string cmdBuff;
-	
-	/* The ID of the child process */
-	pid_t pid;
-	
-	/* Keep running until the user has typed "exit" */
-	do 
-	{
-		/* Prompt the user to enter the command */
-		cerr << "cmd>";
-		cin >> cmdBuff;
-		
-		/* If the user wants to exit */
-		if(cmdBuff != "exit")
-		{
-			/* TODO: Create a child */
-			
-			/* TODO: Error check to make sure the child was successfully created */
-				
-			/*** TODO: If I am child, I will do this: ****/
-			/* Call execlp() to replace my program with that specified at the command line.
-			 * PLEASE NOTE: YOU CANNOT PASS cmdBuff DIRECTLY to execlp(). It is because 
-			 * cmdBuff is an object of type string (i.e., a class) and execlp() expects
- 			 * an array of characters.  However, you can pass cmdBuff.c_str(), which will
-			 * return an array of characters representation of the string object.
-			 * 
-			 * Also, please do not forget to error check your exelp() system calls.
-			 */
-			
-			
-			/*** TODO: If I am a parent, I will do the following ***/
-			/* Wait for the child process to terminate */
-		}
-	}
-	while(cmdBuff != "exit");
-	
-	return 0;	
+    /* The command buffer */
+    string cmdBuff;
+
+    /* The ID of the child process */
+    pid_t pid;
+
+    /* Keep running until the user has typed "exit" */
+    do 
+    {
+        /* Prompt the user to enter the command */
+        cerr << "cmd>";
+        cin >> cmdBuff;
+
+        /* If the user wants to exit */
+        if(cmdBuff != "exit")
+        {
+            /* TODO: Create a child */
+            pid = fork();
+
+            /* TODO: Error check to make sure the child was successfully created */
+            if(pid < 0)
+            {
+                perror("fork:");
+                exit(1);
+            }
+
+            /*** TODO: If I am child, I will do this: ****/
+            /* Call execlp() to replace my program with that specified at the command line.
+             * PLEASE NOTE: YOU CANNOT PASS cmdBuff DIRECTLY to execlp(). It is because 
+             * cmdBuff is an object of type string (i.e., a class) and execlp() expects
+             * an array of characters.  However, you can pass cmdBuff.c_str(), which will
+             * return an array of characters representation of the string object.
+             * 
+             * Also, please do not forget to error check your exelp() system calls.
+             */
+            if(pid == 0)
+            {
+                string s = "/bin/" + cmdBuff;
+                if(execlp(s.c_str(),cmdBuff.c_str(),NULL) < 0)
+                {
+                    perror("execlp");
+                    exit(1);
+                }
+                /*** TODO: If I am a parent, I will do the following ***/
+                /* Wait for the child process to terminate */
+                else
+                {
+                    wait(&pid);
+                }
+            }
+        }
+    }
+    while(cmdBuff != "exit");
+
+    return 0;	
 }
