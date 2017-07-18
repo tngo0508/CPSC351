128cecbeac22626f3b6e9d640686345d49ce0f8f
Thomas Ngo <tngo0508@csu.fullerton.edu>
2017-07-18 14:20:20 +0000

    finish serial

--- serial.cpp
+++ serial.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <cstring>
 #include <fstream>
@@ -5,19 +6,18 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <vector>
-using namespace std;
+using namespace std;    
 
 
 // compile & run
 // g++ serial_download.cpp -o Serial
-// time ./Serial urls.txt
+// ./Serial urls.txt
 
 
 
 
 vector<string> urls;
 int count = 0 ;
-
 void load_urls(){
     string urlline;
     ifstream reader("urls.txt");
@@ -25,43 +25,51 @@ void load_urls(){
         cout <<" fail to open the urls.txt file\n";
         exit (1);
     }
-    while (!reader.eof())
-    {
-        reader >>urlline;
-        urls.push_back(urlline);
-        count++;
-        
-    }
+while (!reader.eof())
+{
+reader >>urlline;
+		if(!reader.eof()){
+
+urls.push_back(urlline);
+count++;
+}
+
+ }  
     reader.close();
 }
 void create_child(){
     pid_t pid;
-    for ( int i = 1 ; i < count; i++){
+    for ( int i = 0 ; i < count ; i++){
         pid = fork();
         if ( pid < 0 ){
             cerr <<"cannot fork";
-            exit (1);
+           exit (1);
         }
         else if (pid ==0){
-            cerr<< " # file " << i << ".......downloaing now \n\n ";
-            execlp("/usr/bin/wget", "wget",urls.back().c_str(), NULL);
+    	cerr<< " # file " << i << ".......downloading now \n\n ";
+        if (execlp("/usr/bin/wget", "wget",urls.back().c_str(), NULL)<0){
+                    perror("execlp");
+                                exit(1);
+		}
         }
-        else{
-            urls.pop_back();
-            urls.end();
-            wait(NULL);
-        }
+	else{  
+	urls.pop_back();
+	urls.end();
+	wait(&pid);
+	}
+      
     }
 }
 
 int main(int argc, char* argv[])
 {
     vector<string> urls;
-    load_urls();
-    
-    cerr << count << endl;
+	load_urls();
+	
     create_child();
-    
+	wait(NULL);
+	cout << endl;
+	cout << " ________________DOWNLOADING DONE______________\n\n";
     return 0;
-    
+
 }
